ca87557cfc7344b1b9f9dfadfc6241efa0fa79d3
yaoyu bao <yaobao5354@gmail.com>
2019-08-23 00:29:36 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,28 @@
+require 'pry'
 
+def sort_array_asc(array)
+  array.sort
+end 
+
+def sort_array_desc(array)
+  array.sort.reverse
+end 
+
+def sort_array_char_count(array)
+  array.sort do |char1, char2|
+    char1.length <=> char2.length
+  end 
+end 
+
+def swap_elements(array)
+  new_array = array
+  array.each_with_index do |element, index|
+    if index == 1 
+      new_array[2] = element
+    elsif index == 2
+      new_array[1] = element
+    end
+    binding.pry
+  end
+  new_array
+end 
